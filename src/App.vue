ca65826404a9e097158cec94cd1c068decb74223
Marta Jamróz Kulig <mjamroz2@smi.pl>
2023-08-07 17:59:14 +0200

    fix for Kamil

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,9 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <p>kola</p>
+  <!-- Kamil. ten tag jest tutaj potrzebny do poprawnego dzialania Routingu -->
+  <!-- Swoje html dodawaj do plikuHomeView.vue póki co -->
+  <!-- Plik HomeView.vue zostanie automatycznie zaimportowany poprzez component <RouterView /> -->
+  <!-- W pliku \src\router\index.js jest zdefiniowany cały routing wraz z importem uzywanego componentu HomeView.vue -->
+  <RouterView />
 </template>
