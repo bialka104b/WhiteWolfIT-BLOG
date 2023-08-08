b6e1c54b07be8e853c3483d4d966eff66922108c
Kamil Kida <kamilkbiz9@gmail.com>
2023-08-08 15:28:24 +0200

    Prawie skończony szkielet

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,59 @@
 <template>
-  <div>Hello world</div>
+  <nav>Nawigacja</nav>
+  <header>Nagłówek</header>
+  <main>
+    <section class="section1">
+      <div class="animation">
+        <img src="../images/logo/Short@iwhitewolf.png" alt="" />
+        <p>
+          <span>iWhiteWolf</span> <br />Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Inventore, amet?
+        </p>
+      </div>
+    </section>
+
+    <h1>What we do!!!</h1>
+
+    <section class="section2">
+      <div class="tiles">
+        <div class="square1">
+          <img src="../images/SmallPic/a-notice-2410276_640.png" alt="Podróże" />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div class="square2">
+          <img src="../images/SmallPic/a-heart-2438744_640.png" alt="Serce" />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div class="square1">
+          <img src="../images/SmallPic/chip-1710300_640.png" alt="Procesor" />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div class="square2">
+          <img src="../images/SmallPic/false-2061131_640.png" alt="X" />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div class="square1">
+          <img src="../images/SmallPic/flat-2126880_640.png" alt="Komputer" />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div class="square2">
+          <img src="../images/SmallPic/responsive-2440940_640.png" alt="Urządzenia" />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      </div>
+    </section>
+
+    <h1>CONTACT</h1>
+
+    <section class="section3">
+     <div class="wrapper">
+      <div class="content"><img src="../images/Icon/mail-1454733_640.png" alt="mail">email1@email.com</div>
+      <div class="content"><p>kola</p></div>
+     </div>
+    </section>
+
+    <footer>
+      kola
+    </footer>
+  </main>
 </template>
