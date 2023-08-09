fd6e353693d825534feaadea98b9a369efab00fb
Kamil Kida <kamilkbiz9@gmail.com>
2023-08-09 19:56:23 +0200

    Przerobiona na mobileFirst+kilka innych poprawek.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>Nawigacja</nav>
   <header>Nagłówek</header>
+
   <main>
     <section class="section1">
       <div class="animation">
@@ -17,7 +18,7 @@
     <section class="section2">
       <div class="tiles">
         <div class="square1">
-          <img src="../images/SmallPic/a-notice-2410276_640.png" alt="Podróże" />
+          <img src="../images/SmallPic/a-notice-2410276_640.png" alt="Podróże" class="bas"/>
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
         </div>
         <div class="square2">
@@ -46,14 +47,64 @@
     <h1>CONTACT</h1>
 
     <section class="section3">
-     <div class="wrapper">
-      <div class="content"><img src="../images/Icon/mail-1454733_640.png" alt="mail">email1@email.com</div>
-      <div class="content"><p>kola</p></div>
-     </div>
+      <div class="wrapper">
+        <h2><img src="../images/logo/Short white@iwhitewolf.png" alt="commpany logo" />Address</h2>
+        <div class="content">
+          <img src="../images/Icon/mail-1454733_640.png" alt="mail icon" /><a href="">Mail</a>
+        </div>
+        <div class="content">
+          <p>Rzeszów, ul,Ulica 1/2 <br>00-000</p>
+        </div>
+        <div class="content">
+          <img
+            src="../images/Icon/icon-157358_640.png"
+            alt="telephone icon"
+            style="max-width: 3vh"
+          />
+          +48 000 00 00
+        </div>
+      </div>
+
+      <div class="wrapper">
+        <h2><img src="../images/Icon/communication-1809935_640.png" alt="" class="bas" />Social Media</h2>
+
+        <div class="content">
+          <img src="../images/Icon/facebook.png" alt="facebook icon" /><a href="/">Facebook</a>
+        </div>
+
+        <div class="content">
+          <img src="../images/Icon/tweeter.png" alt="tweeter icon" /> <a href="/">Tweeter</a>
+        </div>
+
+        <div class="content">
+          <img src="../images/Icon/instagram.png" alt="instagram icon" /><a href="/">instagram</a>
+        </div>
+
+        <div class="content">
+          <img src="../images/Icon/link.png" alt="Linkedin icon" /><a href="/">Linkedin</a>
+        </div>
+      </div>
+
+      <div class="wrapper">
+        <h2><img src="../images/Icon/book-2282152_640.png" alt="book icon" /> Blog</h2>
+
+        <div class="content">
+          <a href="">Article 1</a>
+        </div>
+
+        <div class="content">
+          <a href="">Article 2</a>
+        </div>
+      </div>
     </section>
 
     <footer>
-      kola
+      <p>data utworzenia firmy</p>
+      <div>
+        <img src="../images/Icon/copy.png" alt="" />
+        <p>iWhiteWolf.IT</p>
+      </div>
+      <a href="/">Privacy Policy</a>
     </footer>
   </main>
 </template>
