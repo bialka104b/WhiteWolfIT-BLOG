e1f64a3a9c009d65350d19cf17b22cae05f5afc6
Novvac <novvac21@protonmail.com>
2023-08-09 21:14:48 +0200

    feat(#2): Add mobile version of main navigation

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,7 +1,10 @@
 <script setup>
 import { ref } from 'vue'
+import NavItems from './NavItems.vue';
+import NavDrawer from './NavDrawer.vue';
 
-const navItems = ref([
+const drawer = ref(false);
+const nav = ref([
   { text: 'Home', link: '/' },
   {
     text: 'Blog',
@@ -16,49 +19,22 @@ const navItems = ref([
       }
     ]
   },
-  { text: 'Calculator' },
+  { text: 'Calculator', link: '/calculator' },
   { text: 'Sign in' }
 ])
 </script>
 
 <template>
-  <v-app-bar scroll-behavior="hide" height="80" flat>
+  <v-app-bar scroll-behavior="hide" :height="80" flat>
     <v-container class="d-flex align-center h-100 py-2">
       <img alt="logo" src="src/assets/logo-black-short.png" class="w-auto h-75 d-block" />
 
       <v-spacer />
 
-      <v-app-bar-nav-icon class="d-md-none" />
-      <nav class="d-none d-md-block">
-        <v-btn
-          v-for="(item, index) in navItems"
-          :key="index"
-          :to="item.link"
-          variant="plain"
-          flat
-          class="text-body-1"
-          :class="{ 'ml-2': index !== 0 }"
-        >
-          {{ item.text }}
-
-          <template v-if="item.items" #append>
-            <v-icon icon="mdi-chevron-down"></v-icon>
-          </template>
-          <v-menu v-if="item.items" activator="parent" location="bottom center" offset="8">
-            <v-list elevation="0" bg-color="grey-lighten-4" min-width="150" class="py-0">
-              <v-list-item
-                v-for="(subItem, subIndex) in item.items"
-                :key="subIndex"
-                :to="subItem.link"
-                density="compact"
-                class="text-body-2"
-              >
-                {{ subItem.text }}
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </v-btn>
-      </nav>
+      <v-app-bar-nav-icon class="d-md-none" @click="drawer = !drawer" :icon="drawer ? 'mdi-close' : 'mdi-menu'" />
+      <NavItems :nav="nav" class="d-none d-md-block"/>
     </v-container>
   </v-app-bar>
+
+  <NavDrawer :nav="nav" v-model="drawer" />
 </template>
