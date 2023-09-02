9d89f4aaef1c3e27dcdbea137330c5c52d3f3fc2
novvac <novvac21@protonmail.com>
2023-09-02 22:40:10 +0200

    feat(#6): Add navigation guard to dashboard view

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
-// Layouts
-import DefaultLayout from '../layouts/DefaultLayout.vue'
-
-// Views
-import HomeView from '../views/HomeView.vue'
+import { useUserStore } from '@/stores/user.js';
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,8 +17,28 @@ const router = createRouter({
           component: HomeView
         }
       ]
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard-layout',
+      component: () => import('@/layouts/DashboardLayout.vue'),
+      children: [
+        {
+          path: '',
+          name: 'dashboard',
+          component: () => import('@/views/Dashboard/index.vue')
+        }
+      ]
     }
   ]
+})
+
+router.beforeResolve((to, from) => {
+  const { userLoggedIn } = useUserStore();
+
+  if(!userLoggedIn && to?.matched[0].name === 'dashboard-layout') {
+    return { name: 'home' };
+  }
 })
 
 export default router
