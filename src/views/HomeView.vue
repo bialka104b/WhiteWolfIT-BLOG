16aecd8b3be9b0157e9ed4ead0b2a56667cb579e
Damian Tadla <damiantadla@gmail.com>
2023-09-08 18:14:28 +0200

    fix confict

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,15 +5,11 @@ import Footer from '@/components/Footer.vue'
 </script>
 
 <template>
-<<<<<<< HEAD
   <Header />
   <Main />
+  <Home />
+  <Carousel />
+  <WhatWeDo />
+  <Contact />
   <Footer />
-=======
-  <Home/>
-  <Carousel/>
-  <WhatWeDo/>
-  <Contact/>
-  <Footer/>
->>>>>>> 4817f35b42a83fb53a15dfa291885b603f105c1f
 </template>
