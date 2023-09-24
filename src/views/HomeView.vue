d32db8ad79f11bc8c32d6be43d319d6757413c3a
Marta Jamróz Kulig <mjamroz2@smi.pl>
2023-09-24 13:00:20 +0200

    fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,44 +1,40 @@
-
-
 <template>
-  <nav>Nawigacja</nav>
-  
-
- 
-  <Header />
-  <Main />
-  <Contact />
-  <Baton :text="'Kliknij mnie!'" />
-  <Baton :text="'jbdfaho hnio'" colors="red" />
+	<nav>Nawigacja</nav>
+	<Header />
+	<Main />
+	<Contact />
+	<Baton :text="'Kliknij mnie!'" />
+	<Baton :text="'jbdfaho hnio'" colors="red" />
 </template>
-
-
-
 
 <script>
 import Header from "@/components/Header.vue";
 import Main from "@/components/Main.vue";
 import Contact from "@/components/Contact.vue";
-    import Baton from '@/components/CustomButtons.vue'
-    import { ref, defineComponent } from 'vue'
-  export default defineComponent({
-    components:{
-      Baton
-    },
-    setup(){
-      const showfacebook=ref(false)
-      function changeshowfacebook(){
-        showfacebook.value=true
-      }
+import Baton from "@/components/CustomButtons.vue";
+import { ref, defineComponent } from "vue";
 
-      function removeshowfacebook(){
-        showfacebook.value=false
-      }
-      return{
-        showfacebook,
-        changeshowfacebook,
-        removeshowfacebook
-      }
-    }
-  })
+export default defineComponent({
+	components: {
+		Baton,
+		Header,
+		Main,
+		Contact
+	},
+	setup() {
+		const showfacebook = ref(false);
+		function changeshowfacebook() {
+			showfacebook.value = true;
+		}
+
+		function removeshowfacebook() {
+			showfacebook.value = false;
+		}
+		return {
+			showfacebook,
+			changeshowfacebook,
+			removeshowfacebook
+		};
+	}
+});
 </script>
