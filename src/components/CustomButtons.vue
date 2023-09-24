74580592e883d16bf7975dfa8e6abca6e7d853ef
MilkyWay303 <pomidorowy999@gmail.com>
2023-09-24 12:04:03 +0200

    buttons

---
+++ src/components/CustomButtons.vue
@@ -0,0 +1,34 @@
+<script>
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+    name:"Baton", 
+    props:{
+        text:{
+            type:String
+        },
+    colors:{
+        type:String
+    },
+    },
+    setup(props){
+        const click=ref("Kliknij mnie!")
+        const colors=ref(props.colors)
+        function abc(){
+            console.log(colors)
+        }
+        return{
+            click,
+            colors,
+            abc
+        }
+    }
+})
+
+</script>
+<template>
+    <button @click="abc()" :style="`background:${colors}`">
+        {{ text }}
+    </button>
+    
+</template>
